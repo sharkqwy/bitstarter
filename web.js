6c2c302eca4e38c0e51adc3f6f37dd818cb05a64
Kai Xia <sharkqwy@gmail.com>
2014-03-02 20:03:00 +0000

    web.js debugging1

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html").toString);
+  response.send(fs.readFileSync("index.html").toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
